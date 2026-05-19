6d80387eddc7d765db4e721462fb0b0c0fe959a9
JohnmaDev <jhonechavarria0506@gmail.com>
2026-05-19 10:00:42 -0500

    chore(ui): actualizar favicon por logo de la marca PersonalBarber

--- front-nuxt/nuxt.config.ts
+++ front-nuxt/nuxt.config.ts
@@ -71,7 +71,7 @@ export default defineNuxtConfig({
         { name: 'twitter:card', content: 'summary_large_image' },
       ],
       link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/PersonalBarber.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap' },
